4000d3ae4256210eb7b294ce7a3393cd6083c420
Oliver Zeidler <oliverzeidler97@gmail.com>
2022-10-19 15:41:24 +0200

    Now included new transports by skretzo's shortest path plugin which can be deserialized by DataDeserializer

--- src/myImplementation/Main.java
+++ src/myImplementation/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-
         final GraphBuilder builder = new GraphBuilder();
         final Graph graph = builder.buildGraph();
 
