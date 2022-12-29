aa549f6824f9690ee45616df56b9fe743bc6bd09
Oliver Zeidler <oliverzeidler97@gmail.com>
2022-12-29 17:28:30 +0100

    http json api endpoints now use a generic JsonHttpHandler superclass which handles json (de)serialization and errors

--- src/main/java/wiki/runescape/oldschool/server/PathHttpHandler.java
+++ src/main/java/wiki/runescape/oldschool/server/PathHttpHandler.java
@@ -1,70 +1,36 @@
 package wiki.runescape.oldschool.server;
 
-import com.google.gson.Gson;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import wiki.runescape.oldschool.logic.*;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.time.LocalTime;
-
-public class PathHttpHandler implements HttpHandler {
+public class PathHttpHandler extends JsonHttpHandler<PathRequestJson> {
 
     private final Graph graph;
     private final PathFinder pathFinder;
-    private final Gson gson = new Gson();
 
     public PathHttpHandler(Graph graph, PathFinder pathFinder) {
+        super(PathRequestJson.class);
         this.graph = graph;
         this.pathFinder = pathFinder;
     }
 
     @Override
-    public void handle(final HttpExchange exchange) throws IOException {
-        System.out.println(LocalTime.now() + " " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
-
-        final PathRequestJson requestBody;
-        try (final Reader requestBodyReader = new InputStreamReader(exchange.getRequestBody())){
-            requestBody = gson.fromJson(requestBodyReader, PathRequestJson.class);
-        } catch (Exception e) {
-            returnError(exchange, "Could not parse request body");
-            throw new RuntimeException(e);
-        }
+    protected Reply handle(final Request<PathRequestJson> request) {
+        PathRequestJson requestBody = request.requestBody();
 
         if(requestBody.from() == null || requestBody.to() == null || requestBody.blacklist() == null) {
-            returnError(exchange, "Request body is missing fields");
+            return new Reply(true, "Request body is missing fields");
         }
-
         if (!graph.isWalkable(requestBody.from()) && !graph.isWalkable(requestBody.to())) {
-            returnError(exchange, "Start and end tile are not walkable");
-            return;
+            return new Reply(true, "Start and end tile are not walkable");
         }
         if (!graph.isWalkable(requestBody.from())) {
-            returnError(exchange, "Start tile is not walkable");
-            return;
+            return new Reply(true, "Start tile is not walkable");
         }
         if (!graph.isWalkable(requestBody.to())) {
-            returnError(exchange, "End tile is not walkable");
-            return;
+            return new Reply(true, "End tile is not walkable");
         }
 
         final PathFinderResult path = pathFinder.findPath(graph, requestBody.from(), requestBody.to(), requestBody.blacklist());
-        final byte[] response = gson.toJson(path).getBytes();
-        exchange.getResponseHeaders().add("Content-Type", "application/json");
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-        exchange.sendResponseHeaders(200, response.length);
-        exchange.getResponseBody().write(response);
-        exchange.close();
-
-    }
-
-    private static void returnError(final HttpExchange exchange, final String errorMessage) throws IOException {
-        final byte[] errorMessageBytes = errorMessage.getBytes();
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-        exchange.sendResponseHeaders(400, errorMessageBytes.length);
-        exchange.getResponseBody().write(errorMessageBytes);
-        exchange.close();
+        return new Reply(false, path);
     }
 }
--- src/main/java/wiki/runescape/oldschool/server/TransportsTeleportsHttpHandler.java
+++ src/main/java/wiki/runescape/oldschool/server/TransportsTeleportsHttpHandler.java
@@ -1,33 +1,21 @@
 package wiki.runescape.oldschool.server;
 
-import com.google.gson.Gson;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import wiki.runescape.oldschool.logic.Graph;
 
-import java.io.IOException;
-import java.time.LocalTime;
 import java.util.Collection;
 
-public class TransportsTeleportsHttpHandler implements HttpHandler {
+public class TransportsTeleportsHttpHandler extends JsonHttpHandler<Void> {
 
     private final Graph graph;
-    private final Gson gson = new Gson();
 
     public TransportsTeleportsHttpHandler(final Graph graph) {
+        super(Void.class);
         this.graph = graph;
     }
 
     @Override
-    public void handle(final HttpExchange exchange) throws IOException {
-        System.out.println(LocalTime.now() + " " + exchange.getRequestURI() + " " + exchange.getRequestURI());
-
+    protected Reply handle(final Request<Void> request) {
         Collection<String> allTransportsTeleports = graph.getAllTeleportsTransports();
-        final byte[] response = gson.toJson(allTransportsTeleports).getBytes();
-        exchange.getResponseHeaders().add("Content-Type", "application/json");
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-        exchange.sendResponseHeaders(200, response.length);
-        exchange.getResponseBody().write(response);
-        exchange.close();
+        return new Reply(false, allTransportsTeleports);
     }
 }
